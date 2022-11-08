9e29e17509ce3b54665add4c41861545a0b51eca
AgathePons <ponsagathe37@gmail.com>
2022-11-08 10:05:21 +0100

    ADD: test integers, test throws arithmetic error

--- src/test/java/learn/TestIntegers.java
+++ src/test/java/learn/TestIntegers.java
@@ -1,5 +1,40 @@
 package learn;
 
-public class TestIntegers {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestIntegers {
+    @Test
+    void testSignedInts() {
+        int x = 2000000000; // int signed about between -2 milliards +2 milliards
+        // int x = 3000000000;
+        System.out.println(x);
+        System.out.println(-x);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+    @Test
+    void testComputeOverflowModulo() {
+        int x = 2000000000;
+        int y = x * 2;
+        System.out.println(y); //
+        x = Integer.MAX_VALUE;
+        y = x + 1;
+        System.out.println(y);
+        // When we compute an overflow value, we loop on the range values of the
+        // integer, so Integer.MAX_VALUE + 1 = Integer.MIN_VALUE
+    }
+
+    @Test
+    void testComputeOverflowError() {
+        int x = Integer.MAX_VALUE;
+        // int y = Math.addExact(x, 1); // we force to not loop, so it throws an error
+        ArithmeticException exception = assertThrows(
+                ArithmeticException.class,
+                () -> Math.addExact(x, 1)
+        );
+        assertEquals("integer overflow", exception.getMessage());
+    }
 }
